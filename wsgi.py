494c2d162cdcbbf3282e7232f28bdd22f19881e9
Biswajit Mohapatra <105672728+Biswajit-Mohapatra2@users.noreply.github.com>
2024-08-02 10:55:07 +0000

    port changed

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
 from app import app
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=443)  # Change the port number as needed
+    app.run(host="0.0.0.0", port=8000)  # Change the port number as needed
